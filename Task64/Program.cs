9096fd9a6f75eaa74b00b2b1ef5458dafb494a83
Galprim <galprimulus@gmail.com>
2022-05-28 01:16:21 +0300

    add Task 64

---
+++ Task64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64: Напишите программу, 
+// которая будет принимать на вход число и возвращать сумму его цифр.
+
+int sum = 0;
+void SummOfNumbers(int num)
+{
+    if (num != 0)
+    {
+        sum += num % 10;
+        num = num / 10;
+        SummOfNumbers(num);
+    }
+    else System.Console.WriteLine(sum);
+}
+
+System.Console.Write("Введите любое натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+SummOfNumbers(number);
